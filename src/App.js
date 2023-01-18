89523a789e5ea24a39aa43d5817e99f5313da283
Chris00-lab <christelgabriel00@gmail.com>
2023-01-18 06:02:42 +0200

    update code

--- src/App.js
+++ src/App.js
@@ -1,18 +1,9 @@
-// @bekbrace
-// FARMSTACK Tutorial - Sunday 13.06.2021
- 
-import React, { useState, useEffect} from 'react';
+
 import './App.css';
-import TodoView from './components/TodoListView';
-import axios from 'axios';
 import 'bootstrap/dist/css/bootstrap.min.css'; 
 
 
-
-    
-
- 
-
+function App (){
   return (
     <div className="App list-group-item  justify-content-center align-items-center mx-auto" style={{"width":"400px", "backgroundColor":"white", "marginTop":"15px"}} >
       <h1 className="card text-white bg-primary mb-1" styleName="max-width: 20rem;">Welcome to </h1>
@@ -22,4 +13,6 @@ import 'bootstrap/dist/css/bootstrap.min.css';
       <h6 className="card text-dark bg-warning py-1 mb-0" >Copyright 2023, All rights reserved &copy;</h6>
     </div>
   );
+}
 
+export default App;
