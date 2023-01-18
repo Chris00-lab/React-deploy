6413692fd8076056e453b13e3d98ac036359871f
Chris00-lab <christelgabriel00@gmail.com>
2023-01-18 06:06:32 +0200

    update code

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 
 import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css'; 
+
 
 
 function App (){
   return (
-    <div className="App list-group-item  justify-content-center align-items-center mx-auto" style={{"width":"400px", "backgroundColor":"white", "marginTop":"15px"}} >
-      <h1 className="card text-white bg-primary mb-1" styleName="max-width: 20rem;">Welcome to </h1>
-      <h6 className="card text-white bg-primary mb-3"> React App</h6>
+    <div  >
+      <h1 >Welcome to </h1>
+      <h6 > React App</h6>
      
       <br/>
-      <h6 className="card text-dark bg-warning py-1 mb-0" >Copyright 2023, All rights reserved &copy;</h6>
+      <h6  >Copyright 2023, All rights reserved &copy;</h6>
     </div>
   );
 }
